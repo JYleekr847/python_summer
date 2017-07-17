3e3c54197a58aa9f25960890863033a4e92069c9
DESKTOP-GQ09K1R\lee <junyoung847@naver.com>
2017-07-18 03:37:05 +0900

    2장 마무리

---
+++ Chapter2/Chapter2_bool.py
@@ -0,0 +1,12 @@
+﻿# bool은 참과 거짓을 나타내는 자료형, True 와 False가 사용가능하다. 
+
+isRight = False
+print(type(isRight))
+print( 1<2, 1!=2, 1==2)
+a = True and False
+b = True & True
+c = True or False
+d = False | False
+e = not True
+
+print(a,b,c,d,e)
---
+++ Chapter2/Chapter2_dic.py
@@ -0,0 +1,36 @@
+﻿#  사전은 키와 값으로 쌍으로 이루어져 있습니다.
+
+d = dict(a=1,b=2,c=3)
+print(d, type(d))
+
+# dict() 생성자 없이도 직접 사전을 생성할 수 있습니다.
+
+colors = {"apple" : "red", "banana":"yellow" }
+print(colors)
+a = colors["apple"]
+print(a)
+
+# 사전에 새로운 값을 추가하는 방법 
+
+colors["cherry"] = "red"
+colors["melon"] = "green"
+print(colors)
+
+# 사전의 내용을 얻는 방법 - items() , keys(), values()
+
+for c in colors.items():
+	print(c)
+
+for k, v in colors.items():
+	print(k,v)
+
+for k in colors.keys():
+	print(k)
+
+for v in colors.values():
+	print(v)
+
+# 사전의 내용의 삭제 - 'del'
+
+del colors['cherry']
+print(colors)
