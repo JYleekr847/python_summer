5ac5abe7ea9604180a2de1af5e32d4cd3bd08a29
DESKTOP-GQ09K1R\lee <junyoung847@naver.com>
2017-07-19 01:31:49 +0900

    챕터3 스코핑룰

---
+++ Chapter3/2.py
@@ -0,0 +1,35 @@
+﻿# 파이썬에서 인자는 레퍼런스를 이용해 전달한다. 
+a = 10
+b= 20 
+
+def sum1(x, y):
+	return x+y
+print(sum1(a,b))
+
+#정수형 변수는 변경 불가능한 변수 
+x = 10 
+def sum2(x, y):
+	x = 1
+	return x+y
+print(sum2(x,b))
+print(x)
+
+# 리스트는 변경가능한 변수타입
+# 함수가 종료된뒤 함수 내에서 함수 바깥의 값을 변경시킴, x[0]는 레퍼런스가 아닌 실제값의 주소를 의미한다.
+def change(x):
+	x[0] ='H'
+wordlist = ['J','A','M']
+change(wordlist)
+print(wordlist)
+
+# 변경가능한 변수타입을 함수내에서 변경했을때 외부에 영향을 미치지 않게하는 방법 
+
+def change2(x):
+	x = x[:]
+	x[0] = 'H'
+	return None
+wordlist = ['J','A','M']
+change2(wordlist)
+print(wordlist)
+
+# 깊은 복사를 이용해 똑같은 객체를 한개 더 생성한다.
---
+++ Chapter3/return.py
@@ -0,0 +1,40 @@
+﻿def Times(a,b):
+	return a*b
+print(Times(10,10))
+
+# 함수의 레퍼런스를 다른 변수에 할당하게 되면 함수 객체가 복사되는 것이 아니라 레퍼런스만 복사되는것(얕은 복사)
+myTimes = Times
+
+#위와 같은 경우에는 myTimes와 Times가 같은 함수 객체를 가리키게 된다.
+
+r = myTimes(10,10)
+print(r)
+
+# return은 반환값으로 오직 한개의 객체만 반환가능하다. 튜플을 이용하여 여러개를 동시에 반환하는 것처럼 보이기도 한다.
+
+def swap(x,y):
+	return y,x
+print(swap(1,2))
+a, b = swap(1,2)
+print(a, b)
+x= swap(1,2)
+print(type(x)) 
+	
+
+def intersect(prelist, postlist):
+	retList = []
+	for x in prelist:
+		if x in postlist and x not in retList:
+			retList.append(x)
+	return retList
+list1 = "SPAM"
+list2 = "EGG"
+is1 = intersect(list1, list2)
+print(is1)
+is2 = intersect(list1, ['H','A','M'])
+print(is2)
+tuple1 = ('B' , 'E', 'E', 'F')
+is3 = intersect(list2, tuple1)
+print(is3)
+
+
