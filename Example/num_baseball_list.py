fbbc85463f736faaf878f9cf4b2a232ae277b212
DESKTOP-GQ09K1R\lee <junyoung847@naver.com>
2017-07-28 01:45:49 +0900

    숫자야구 리스트, 튜플

---
+++ Example/num_baseball_list.py
@@ -0,0 +1,20 @@
+﻿num = input("숫자를 입력하세요")
+num= list(num)
+ball = 0
+strike = 0
+while True:
+    num_input = input( " 숫자를 예측하세요 ")
+    num_input = list(num_input)
+    for i in range(3):
+        for j in range(3):
+            if int(num[i]) == int(num_input[j]) :
+                if i == j :
+                    strike = strike + 1
+                else:
+                    ball = ball + 1
+    print("strike : {0} , ball : {1} ".format(strike,ball))
+    if strike == 3:
+        break
+    ball = 0
+    strike = 0
+print(" 정답을 맞췄습니다")
